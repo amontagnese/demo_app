d9c22d6f83fba608b38e369ac2e30e28f15a6c57
amontagnese <amontagnese@grio.com>
2012-01-11 17:31:52 -0800

    couch db gem added

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.3'
 
 gem 'sqlite3'
 gem 'capistrano'
+gem 'couchrest_model'
 
 
 # Gems used only for assets and not required
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "demo_app"
+set :repository,  "git@github.com:amontagnese/demo_app.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "http://ec2-50-18-64-240.us-west-1.compute.amazonaws.com/"                          # Your HTTP server, Apache/etc
+role :app, "http://ec2-50-18-64-240.us-west-1.compute.amazonaws.com/"                          # This may be the same as your `Web` server
+role :db,  "http://ec2-50-18-64-240.us-west-1.compute.amazonaws.com/", :primary => true # This is where Rails migrations will run
+
+set :user, "bitnami"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
